9c573140064a342c5489f48bac66d11076189a71
akindutire <akinsamuel33@gmail.com>
2020-01-25 13:56:55 +0100

    wrapped acceptance of input in a function

--- blockchain.py
+++ blockchain.py
@@ -8,13 +8,18 @@ def add_block(data = 0.1, last_block = [1] ):
     #Append the last block into the new block and then the new value for the current block
     blockchain.append( [last_block, data ])
 
+def accept_block_data():
+    #Returns a string, but to be casted to float
+    tx_amount = input("Please enter transaction data/amount ")
+    return float(tx_amount)
+
 
 add_block(4.5)
-add_block(3.5, get_last_block_of_chain())
 
-#Returns a string, but to be casted to float
-tx_amount = input("Please enter transaction data/amount ")
+tx_amount = accept_block_data();
+add_block(tx_amount, get_last_block_of_chain())
 
+tx_amount = accept_block_data();
 add_block(last_block = get_last_block_of_chain(), data = float(tx_amount) ) 
 
 print(blockchain)
