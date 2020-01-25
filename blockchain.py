3b1890f1048a7a22f09b039ebcf19ac11b222c22
akindutire <akinsamuel33@gmail.com>
2020-01-25 16:41:46 +0100

    used the none datatype

--- blockchain.py
+++ blockchain.py
@@ -2,10 +2,14 @@ blockchain = [ ]
 
 def get_last_block_of_chain():
     #blockchain[-1] is the last block of the blockchain, however [1] is the genesis block of the blockchain
+    if len(blockchain) < 1:
+        return None;
     return blockchain[-1]
 
-def add_block(data = 0.1, last_block = [1] ):
+def add_block(data = 0.1, last_block):
     #Append the last block into the new block and then the new value for the current block
+    if last_block == None:
+        last_block = [1]
     blockchain.append( [last_block, data ])
 
 def accept_block_data():
@@ -14,7 +18,7 @@ def accept_block_data():
     return float(tx_amount)
 
 #Add genesis block
-add_block() 
+add_block(last_block = get_last_block_of_chain()) 
 
 """ 
     Understanding loops 
