b68d1885e65f24565ec995405c6ab392f70e7316
akindutire <akinsamuel33@gmail.com>
2020-01-25 13:39:25 +0100

    removed genesis block from the chain and pass is as default argument in the add_block function

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,16 @@
-blockchain = [ [1] ]
+blockchain = [ ]
 
 def get_last_block_of_chain():
+    #blockchain[-1] is the last block of the blockchain, however [1] is the genesis block of the blockchain
     return blockchain[-1]
 
-def add_block(data):
+def add_block(data = 0.1, last_block = [1] ):
     #Append the last block into the new block and then the new value for the current block
-    #blockchain[-1] is the last block of the blockchain, however [1] is the genesis block of the blockchain
-    blockchain.append( [get_last_block_of_chain(), data ])
-    print(blockchain)
+    blockchain.append( [last_block, data ])
+
 
 add_block(4.5)
-add_block(3.5)
+add_block(3.5, get_last_block_of_chain())
+add_block() 
+
+print(blockchain)
