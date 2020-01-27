1e7f6c9af3c637625574335650edbf3afe09e4b2
akindutire <akinsamuel33@gmail.com>
2020-01-27 19:10:29 +0100

    learnt about complex iterable data structures and their properties

---
+++ iterable.py
@@ -0,0 +1,12 @@
+#list, set, tuple,
+# A list is more like an array in other PL  
+f_lsit = ['Milkish', 'Milkish', 'Salty']
+
+#SET doesn't allow duplicated data but mutated
+f_set = { 'Milkish', 'Milkish', 'Salty' }
+
+#TUPLE:immutatble are for hard coded list with duplicated entries in a particular order
+f_tuple = ( 'Malty', 'Salty', 'Milkish')
+
+#DICTIONARY is mutatble, it just like Javascript objects
+f_dic = { 'name' : 'Milk', 'n': 2 }
