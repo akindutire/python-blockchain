facc8ffd0e7cd937d8c768ed2a275fe87878fe7e
akindutire <akinsamuel33@gmail.com>
2020-01-28 20:05:13 +0100

    learnt about enumeration to cast element of a list to a tuple with index and the elements

--- iterable.py
+++ iterable.py
@@ -7,6 +7,11 @@ s_list = [1,2,3,4,5]
 s2_list = [ element * 2 for element in s_list ]
 print(s2_list)
 
+#converting a list to a list of tuples using enumerate
+tupled_list = enumerate(s2_list)
+for (index, element) in tupled_list:
+    print(element)
+
 #SET doesn't allow duplicated data but mutated
 f_set = { 'Milkish', 'Milkish', 'Salty' }
 
