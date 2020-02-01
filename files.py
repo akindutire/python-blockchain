e017ca691124f61fba3ce72b8358120944b18f61
akindutire <akinsamuel33@gmail.com>
2020-02-01 11:54:55 +0100

    explored readlines function

--- files.py
+++ files.py
@@ -1,3 +1,14 @@
+
+#modes, a=append, w=write, r+=read/write, b=read binary, r=read only
+
 file_handle = open('data/heck.txt', mode='w')
 file_handle.write("Hello here")
-file_handle.close()
+file_handle.close()    
+
+file_handle = open('data/heck.txt', mode='r')
+#All contents on different lines as a string
+contents = file_handle.read()
+
+#all contents on diff. lines as list
+contents = file_handle.readlines()
+print(contents)
