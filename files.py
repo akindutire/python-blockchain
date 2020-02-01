67e682314429e9a849811d6c00a8740892b1e74e
akindutire <akinsamuel33@gmail.com>
2020-02-01 12:02:37 +0100

    use of with block on files

--- files.py
+++ files.py
@@ -1,14 +1,19 @@
 
 #modes, a=append, w=write, r+=read/write, b=read binary, r=read only
 
-file_handle = open('data/heck.txt', mode='w')
-file_handle.write("Hello here")
-file_handle.close()    
+with open('data/heck.txt', mode='w') as file_handle:
+    file_handle.write("Hello here")
 
-file_handle = open('data/heck.txt', mode='r')
-#All contents on different lines as a string
-contents = file_handle.read()
 
-#all contents on diff. lines as list
-contents = file_handle.readlines()
-print(contents)
+with open('data/heck.txt', mode='r') as file_handle:
+    #All contents on different lines as a string
+    contents = file_handle.read()
+
+    #all contents on diff. lines as list
+    contents = file_handle.readlines()
+    print(contents)
+
+    line = file_handle.readline()
+    while line:
+        print(line)
+        line = file_handle.readline()
